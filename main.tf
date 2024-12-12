9a4668e118c38edffa9255717a3acd9943d4abcf
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2024-12-12 11:48:41 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,96 +1,70 @@
 provider "aws" {
-        region = "us-east-2"
+  region = "us-east-1"
 }
-        resource "aws_vpc" "Myvpc" {
-                cidr_block = "10.0.0.0/16"
-                tags = {
-                        Name = "Myvpc"
-                }
-        }
-        resource "aws_subnet" "mypublicsubnet" {
-                vpc_id = aws_vpc.Myvpc.id
-                availability_zone = "us-east-2a"
-                cidr_block = "10.0.1.0/24"
-                tags = {
-                        Name = "mypublicsubnet"
-                }
-        }
-        resource "aws_subnet" "myprivatesubnet" {
-                vpc_id = aws_vpc.Myvpc.id
-                availability_zone = "us-east-2a"
-                cidr_block = "10.0.2.0/24"
-                tags = {
-                        Name = "myprivatesubnet"
-                }
-        }
-        resource "aws_internet_gateway" "igw" {
-                vpc_id = aws_vpc.Myvpc.id
-                tags = {
-                        Name = "igw"
-                }
-        }
-        resource "aws_route_table" "mainrtb" {
-                vpc_id = aws_vpc.Myvpc.id
-                route {
-                        cidr_block = "0.0.0.0/0"
-                        gateway_id = aws_internet_gateway.igw.id
-                }
-        }
-        resource "aws_route_table_association" "public_subnet" {
-                subnet_id = aws_subnet.mypublicsubnet.id
-                route_table_id = aws_route_table.mainrtb.id
-        }
-        resource "aws_s3_bucket" "s3_bucket" {
-                bucket = "mys3bucket773773"
-        }
-        resource "aws_instance" "Terraform_dup" {
-                ami = "ami-0f599bbc07afc299a"
-                instance_type = "t2.micro"
-                subnet_id = aws_subnet.mypublicsubnet.id
-                tags = {
-                                        Name = "Terraform_dup"
-                }
-        }
-              resource "aws_key_pair" "TF_key" {
-                key_name = "TF_key"
-                public_key = tls_private_key.rsa.public_key_openssh
-        }
-        resource "tls_private_key" "rsa" {
-                algorithm = "RSA"
-                rsa_bits  = 4096
-        }
-        resource "local_file" "TF_key" {
-                content  = tls_private_key.rsa.private_key_pem
-                filename = "tfkey"
-        }
-        resource "aws_security_group" "sg" {
-                name        = "sg"
-                description = "Allow ssh and tcp inbound traffic"
-                vpc_id      = aws_vpc.Myvpc.id
 
-                ingress {
-                        description      = "Allow only ssh"
-                        from_port        = 22
-                        to_port          = 22
-                        protocol         = "ssh"
-                }
-                ingress {
-                        description      = "Allow only tcp"
-                        from_port        = 80
-                        to_port          = 80
-                        protocol         = "tcp"
-                }
+resource "aws_iam_role" "lambda_execution_role" {
+  name               = "lambda_execution_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 
-                 egress {
-                        from_port        = 0
-                        to_port          = 0
-                        protocol         = "-1"
-                        cidr_blocks      = ["0.0.0.0/0"]
-                        ipv6_cidr_blocks = ["::/0"]
-                  }
-                tags = {
-                            Name = "sg"
-                  }
-                }
+resource "aws_iam_role_policy_attachment" "lambda_cloudwatch_attachment" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
-  
+resource "aws_lambda_function" "slack_notification_function" {
+  filename         = "function.zip" # Pre-packaged zip file containing the Lambda function code
+  function_name    = "slack_notification_function"
+  role             = aws_iam_role.lambda_execution_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  source_code_hash = filebase64sha256("function.zip")
+  environment {
+    variables = {
+      SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
+    }
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "deployment_status_rule" {
+  name        = "deployment_status_rule"
+  description = "Triggers when a deployment event occurs"
+  event_pattern = <<EOF
+{
+  "source": ["aws.codepipeline"],
+  "detail-type": ["CodePipeline Pipeline Execution State Change"]
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.deployment_status_rule.name
+  target_id = "slack_notification_lambda"
+  arn       = aws_lambda_function.slack_notification_function.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.slack_notification_function.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.deployment_status_rule.arn
+}
+
+output "lambda_function_arn" {
+  value = aws_lambda_function.slack_notification_function.arn
+}
